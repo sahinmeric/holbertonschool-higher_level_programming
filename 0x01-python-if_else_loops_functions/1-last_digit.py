29a8b292323dbb9f26d1a633547bafdaaa885ebb
Sahin <sahin.meric86@gmail.com>
2021-12-21 10:25:32 -0800

    negative number cond added

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+if number < 0:
+    cpynumber = number * -1
+cpynumber = number
+last_digit = cpynumber % 10
 _string = ""
 if last_digit > 5:
     _string = "and is greater than 5"
