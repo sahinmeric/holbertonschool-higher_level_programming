ab2edacc48a9c0313a7d7f054e35e4a6e60bb5fc
Sahin <sahin.meric86@gmail.com>
2021-12-22 10:36:56 -0800

    check loop

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "list.h"
+
+/**
+ *check_cycle - checks if there is a loop in given linked list
+ *@head: head of given linked list
+ *Return: 1 if there is a loop 0 if there is not
+ */
+int check_cycle(listint_t *head)
+{
+	listint_t *list1, *list2;
+
+	list1 = head;
+	list2 = head;
+
+	while (list1 != NULL && list2 != NULL && list2->next != NULL)
+	{
+		list1 = list1->next;
+		list2 = list2->next->next;
+		if (list1 == list2)
+			return (1);
+	}
+	return (0);
+}
