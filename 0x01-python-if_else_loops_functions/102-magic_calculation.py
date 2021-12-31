a514e266d603163e032fe95425bb08f86f501a50
Sahin <sahin.meric86@gmail.com>
2021-12-31 11:59:53 -0800

    removed some paranthesis

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b, c):
-    if (a < b):
-        return (c)
-    if (c > b):
-        return(a + b)
+    if a < b:
+        return c
+    if c > b:
+        return a + b
     else:
-        return((a * b) - c)
+        return(a * b - c)
