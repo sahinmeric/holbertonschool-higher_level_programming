6892399ca0c0bb1712d5abfed66c912e429e73df
Sahin <sahin.meric86@gmail.com>
2022-01-03 15:31:41 -0800

    if len matrix == 0 cond added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 0:
+        print()
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             print("{:d}".format(matrix[i][j]),end="")
