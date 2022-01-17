5e6cd95d76c023e204b5308bdaf28f236bfdbaf4
Sahin <sahin.meric86@gmail.com>
2022-01-17 15:49:27 -0800

    another try

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,10 +3,13 @@ def safe_print_list_integers(my_list=[], x=0):
     nmb = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            if type(my_list[i]) != int:
+                continue
+            else:
+                print("{:d}".format(my_list[i]), end="")
             nmb += 1
-    except:
-        continue
+    except IndexError:
+        raise
     finally:
         print()
     return nmb
