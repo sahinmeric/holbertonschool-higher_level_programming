d3a5861e1ec323cfc80cc1e10406210c6420b822
Sahin <sahin.meric86@gmail.com>
2022-01-17 15:16:47 -0800

    except correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             nmb += 1
-    except:
+    except IndexError:
         pass
     finally:
         print()
