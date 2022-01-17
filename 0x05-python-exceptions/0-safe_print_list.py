ae0cc132951d13d43757fda423efb50c14309ed0
Sahin <sahin.meric86@gmail.com>
2022-01-17 15:12:23 -0800

    pycode

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,9 +5,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print(my_list[i], end="")
-            nmb +=1
+            nmb += 1
     except:
         pass
     finally:
         print()
-    return nmb 
+    return nmb
