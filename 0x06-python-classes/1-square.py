39b4477fce3b43f1ee7cd88654f1b19649e564de
Sahin <sahin.meric86@gmail.com>
2022-01-18 13:45:36 -0800

    first try

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A python script defines an empty class named Square"""
+
+
+class Square:
+    """An empty class that defines square"""
+
+    def __init__(self, size):
+        self.__size = size
