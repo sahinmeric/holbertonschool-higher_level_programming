0cac0f8610332fde4c15d3133c556ec34f856ff6
Sahin <sahin.meric86@gmail.com>
2022-01-18 13:29:30 -0800

    deleted extra lines

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,16 +2,5 @@
 """ A python script defines an empty class named Square"""
 
 class Square:
-    """An empty class that defines square
-
-    Note:
-        Some notes
-
-    Args:
-        Arguments
-
-    Attributes:
-        Attributes
-
-    """
+    """An empty class that defines square"""
     pass
