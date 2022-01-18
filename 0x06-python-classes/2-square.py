e8569e59900945b70b37e7b5bfc6c345a6e6530d
Sahin <sahin.meric86@gmail.com>
2022-01-18 13:51:05 -0800

    first try

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A python script defines a class named Square"""
+
+
+class Square:
+    """A class that defines square"""
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            print("size must be an integer")
+            raise TypeError
+        if size < 0:
+            print("size must be >= 0")
+            raise ValueError
+        else:
+            self.__size = size
