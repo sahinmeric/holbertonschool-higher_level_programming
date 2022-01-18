a2ce4d5e480e5d372b74445d3b7cc39995b7fb5a
Sahin <sahin.meric86@gmail.com>
2022-01-17 16:54:53 -0800

    fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     nmb = 0
     try:
         for i in range(x):
-            if (type(my_list[i]) == int):
+            if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
-            nmb += 1
+                nmb += 1
     except IndexError:
         raise
     finally:
