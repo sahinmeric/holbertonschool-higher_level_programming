61ee2b05d8333056dfa6438c503228d7bd48b7e9
Sahin <sahin.meric86@gmail.com>
2022-01-18 17:57:06 -0800

    first try

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" This module contains a class that defines a square
+In the Square class we initialize each object by the __init_ method"""
+
+
+class Square:
+    """A class that defines square"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            print("size must be an integer", end="")
+            raise TypeError
+        elif value < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        else:
+            self.__size = value
+
+    def my_print(self):
+        """ print a square with #"""
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            for j in range(self.size):
+                print("#",end="")
+            print()
