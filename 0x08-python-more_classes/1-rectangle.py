e72e568452e86f6f1edc1b1da8bd272d639dc742
sahinmeric <sahin.meric86@gmail.com>
2022-01-24 23:48:04 -0300

    rectangle class created

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Python script that defines a class named Rectangle and its functions."""
+
+
+class Rectangle:
+    """Class that defines a rectangle with private attributes."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter and setter used to validate type and value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+
+    @property
+    def height(self):
+        """Getter and setter used to validate type and value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
