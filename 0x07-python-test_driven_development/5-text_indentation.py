f3996b4cbb1001244f8e4cb67328b00505592fed
sahinmeric <sahin.meric86@gmail.com>
2022-01-24 21:41:18 -0300

    exercise 4

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -11,8 +11,21 @@ def text_indentation(text):
 	and prints 2 new lines after each of these characters . ? and :
 	@text: given text
 	"""
-	if text is not isinstance(text, str):
+	if not isinstance(text, str):
 		raise TypeError("text must be a string")
-	if text is None or len(text) <=0:
-		raise TypeError("text must be a string")
-	
+	flag = 0
+	for c in text:
+		if flag == 0:
+			if c == '.' or c == ':' or c == '?':
+				print("{}\n".format(c))
+				flag = 1
+			else:
+				print(c, end="")
+				flag = 0
+				continue
+		if flag == 1:
+			if c == '.' or c == ':' or c == '?' or c == ' ':
+				continue	
+			else:
+				print(c, end="")
+				flag = 0
