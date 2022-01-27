3ba418dfb73fe1ba1e2360a9a498d92f8e181ff1
sahinmeric <sahin.meric86@gmail.com>
2022-01-26 21:51:38 -0300

    first commit

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Python script that defines a class named Rectangle and its functions."""
+
+
+class Rectangle:
+    """Class that defines a rectangle with private attributes."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter and setter used to validate type and value of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+
+    @property
+    def height(self):
+        """Getter and setter used to validate type and value of height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return int(self.__width) * int(self.__height)
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.height == 0 or self.width == 0:
+            return 0
+        return (2 * int(self.__width)) + (2 * int(self.__height))
+
+    def __str__(self):
+        return 'Rectangle({self.width},{self.height})'
