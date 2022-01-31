9cf22aadb96c5ab8ee984bd2433f10dc260fdc52
sahinmeric <sahin.meric86@gmail.com>
2022-01-31 19:37:55 -0300

    first try

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" module has a function that checks
+wheter given objects are from same class or not
+@obj: object
+@a_class: class
+"""
+
+
+def is_same_class(obj, a_class):
+    """returns True if the objects belongs to same class,
+    else False"""
+    return(isinstance(obj, a_class))
