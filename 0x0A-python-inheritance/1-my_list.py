32312af064b302f578d9ff4d44a329ebde1f1ae4
sahinmeric <sahin.meric86@gmail.com>
2022-01-31 19:13:48 -0300

    typeerror exception added

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,4 +6,7 @@ class MyList(list):
     """class MyList that inherits from list"""
     def print_sorted(self):
         """that prints the list, but sorted"""
+        for elem in self:
+            if elem is not isinstance(self, int):
+                raise TypeError("list elements must be integer")
         print(sorted(self))
