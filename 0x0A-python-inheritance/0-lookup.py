4f501704c15890f11df3042b4f0bdf41f0900404
sahinmeric <sahin.meric86@gmail.com>
2022-01-31 18:06:59 -0300

    doc fix

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,8 +2,10 @@
 """
 This module contains a function
 that returns attributes of given class
+@obj: object
 """
 
 
 def lookup(obj):
+    """ returns given objects attibutes and methods"""
     return(dir(obj))
