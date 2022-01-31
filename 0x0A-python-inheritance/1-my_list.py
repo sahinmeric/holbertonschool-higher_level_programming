01c1633ef6887381c16c06c972821e70d4c4350f
sahinmeric <sahin.meric86@gmail.com>
2022-01-31 19:07:15 -0300

    stash

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,5 +5,5 @@
 class MyList(list):
     """class MyList that inherits from list"""
     def print_sorted(self):
-        """that prints the list, but sorted"""
+        """prints the list, but sorted"""
         print(sorted(self))
