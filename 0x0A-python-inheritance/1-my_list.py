6f04b4a105922645908e7e18f25231ff9d6345f6
sahinmeric <sahin.meric86@gmail.com>
2022-01-31 19:18:45 -0300

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,11 +2,14 @@
 """initialization"""
 
 
+from xml.dom.minidom import Element
+
+
 class MyList(list):
     """class MyList that inherits from list"""
     def print_sorted(self):
         """that prints the list, but sorted"""
-        for elem in self:
-            if elem is not isinstance(self, int):
-                raise TypeError("list elements must be integer")
+        for item in self:
+            if not isinstance(item, int):
+                raise TypeError("list items must be integer")
         print(sorted(self))
