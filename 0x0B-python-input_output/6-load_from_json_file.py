e1349c6f0b41809d64e45324edd7f58828e1f69a
sahinmeric <sahin.meric86@gmail.com>
2022-02-01 19:00:38 -0300

    loads

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+initialize
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+    creates an Object from a “JSON file”:
+    @filename: file
+    """
+    with open(filename, 'w') as f:
+        f.write(json.loads(filename))
