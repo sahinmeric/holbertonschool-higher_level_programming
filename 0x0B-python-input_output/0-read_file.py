fc230dc82433efd96a304403a12df2434b2be943
sahinmeric <sahin.meric86@gmail.com>
2022-02-01 17:32:04 -0300

    first try

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 """
-
+This module includes a function that
+reads a file and prints context to stdout.
 """
 
 
 def read_file(filename=""):
-    pass
+    """reads a text file (UTF8) and prints it to stdout:
+    @filename: file that will be read
+    """
+    with open(filename) as f:
+        print(f.read())
+        f.close()
