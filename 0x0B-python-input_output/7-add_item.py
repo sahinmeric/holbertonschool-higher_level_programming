b6993b26368e083b17d7177ab52cfbe419d93c04
sahinmeric <sahin.meric86@gmail.com>
2022-02-01 23:42:09 -0300

    try except

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,8 +11,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-my_list = load_from_json_file("add_item.json")
-
+try:
+    my_list = load_from_json_file("add_item.json")
+except:
+    my_list = []
 for i in range(1, len(argv)):
     my_list.append(argv[i])
 save_to_json_file(my_list, "add_item.json")
