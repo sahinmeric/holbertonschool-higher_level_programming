12d03d133353c6569fbc8840be885fe9e1a101ec
sahinmeric <sahin.meric86@gmail.com>
2022-02-03 22:54:44 -0300

    base class created

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+this module contains base class
+"""
+
+
+class Base:
+    """
+    Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+    
