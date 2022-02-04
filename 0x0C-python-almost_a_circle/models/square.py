5717a7cf4e0a01453ecea7d58d336d46e1ea9700
sahinmeric <sahin.meric86@gmail.com>
2022-02-04 20:42:52 -0300

    square class created

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+this module contains a class named square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.width))
