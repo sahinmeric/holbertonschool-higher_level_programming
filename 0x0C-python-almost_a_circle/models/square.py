d62175e9bc1f6e830f91dd2e6111fbe064a0745e
sahinmeric <sahin.meric86@gmail.com>
2022-02-04 22:08:53 -0300

    update method added with kwargs

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,3 +35,25 @@ class Square(Rectangle):
                 raise ValueError("size must be > 0")
         else:
             raise TypeError("width must be an integer")
+
+    def update(self, *args, **kwargs):
+        if len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.size = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                if key == 'size':
+                    self.size = value
+                if key == 'x':
+                    self.x = value
+                if key == 'y':
+                    self.y = value
