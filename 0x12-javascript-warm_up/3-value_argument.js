055250b74204494774139140e5dde8c06a91f890
sahinmeric <sahin.meric86@gmail.com>
2022-04-04 16:50:59 -0300

    concat

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
+const args = process.argv;
+if (args.length <= 2) {
   console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
