4330bc9aa44e91db33ad5a65780f11f2065fadd7
sahinmeric <sahin.meric86@gmail.com>
2022-04-04 22:46:09 -0300

    another

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < array.length; i++) {
+for (let i = 0; i < array[i]; i++) {
   console.log(array[i]);
 }
