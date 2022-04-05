922fa7657ddc9eb508580e04cd8c5422b0248c67
sahinmeric <sahin.meric86@gmail.com>
2022-04-04 22:43:08 -0300

    another try

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const value of array) {
-  console.log(value);
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
 }
