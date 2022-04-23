4f91e3e067166159acdfedbc4cc15f5960e2f5d1
sahinmeric <sahin.meric86@gmail.com>
2022-04-23 17:16:35 -0300

    requests import fix

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 """
 Python script that fetches https://intranet.hbtn.io/status
 """
-from urllib import requests
+import requests
 
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
