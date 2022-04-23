26f1445946fa94810b777c2745d6a8fbecc034fd
sahinmeric <sahin.meric86@gmail.com>
2022-04-22 22:50:04 -0300

    json fix

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This bash script sends a POST request to the URL passed in the form of JSON data, and displays the body of the response.
-curl -X POST -sH 'Content-Type: application/json'" -d "$2" "$1"
+curl -X POST -sH 'Content-Type: application/json' -d "$2" "$1"
