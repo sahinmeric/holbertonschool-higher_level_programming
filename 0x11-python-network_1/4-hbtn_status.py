94d0c52c773f0840ef2e54c54e1446d797ebacc4
sahinmeric <sahin.meric86@gmail.com>
2022-04-24 21:29:33 -0300

    upt

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,6 +3,7 @@
 Python script that fetches https://intranet.hbtn.io/status
 """
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     req = requests.get("https://intranet.hbtn.io/status")
