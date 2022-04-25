6bf408234686c02bf7ce89e4439e15e04b309061
sahinmeric <sahin.meric86@gmail.com>
2022-04-24 22:00:05 -0300

    error

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    req = requests.get(argv[1])
+    if req.status_code > 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
